5735e94dcb060fe87ba38467b8712b95fca7c47c
OLukyanenko91 <olukyanenko91@gmail.com>
2025-01-18 15:15:18 +0100

    Add Logger

---
+++ Logger/Logger.cpp
@@ -0,0 +1,106 @@
+#include <cassert>
+#include "Logger.hpp"
+
+
+///// LOGGER_STREAM /////
+
+Logger::LoggerStream::LoggerStream(const eP7Trace_Level level,
+                                   const std::string& tag,
+                                   const uint16_t line,
+                                   const std::string& file,
+                                   const std::string& function)
+    : mLevel(level)
+    , mTag(tag)
+    , mLine(line)
+    , mFile(file)
+    , mFunction(function)
+    , mStream()
+{}
+
+Logger::LoggerStream::~LoggerStream()
+{
+    Logger::Instance().P7Log(mLevel,
+                             mTag,
+                             mLine,
+                             mFile,
+                             mFunction,
+                             mStream.str());
+}
+
+///// LOGGER /////
+
+Logger::LoggerStream Logger::Log(const eP7Trace_Level level,
+                                 const uint16_t line,
+                                 const std::string& file,
+                                 const std::string& function,
+                                 const std::string& module)
+{
+    return LoggerStream(level,
+                        module,
+                        line,
+                        file,
+                        function);
+}
+
+Logger& Logger::Instance()
+{
+    static Logger logger;
+    return logger;
+}
+
+Logger::Logger()
+{
+    Init();
+}
+
+Logger::~Logger()
+{
+    if (mP7Trace)
+    {
+        mP7Trace->Unregister_Thread(0);
+    }
+}
+
+void Logger::Init()
+{
+    mP7Client.reset(P7_Create_Client(TM("/P7.Pool=32768")));
+    if (mP7Client)
+    {
+        std::cerr << "Fail to create P7 client" << std::endl;
+        return;
+    }
+
+    mP7Trace.reset(P7_Create_Trace(mP7Client.get(), TM("Trace channel 1")));
+    if (!mP7Trace)
+    {
+        std::cerr << "Fail to create P7 trace" << std::endl;
+        return;
+    }
+
+    mP7Trace->Register_Thread(TM("Application"), 0);
+}
+
+void Logger::P7Log(const eP7Trace_Level level,
+                 const std::string& module,
+                 const uint16_t line,
+                 const std::string& file,
+                 const std::string& function,
+                 const std::string& message)
+{
+    if (!mP7Trace) {
+        return;
+    }
+
+    if (mP7Modules.find(module) == mP7Modules.end()) {
+        mP7Modules[module] = IP7_Trace::hModule();
+        mP7Trace->Register_Module(TM(module.c_str()), &mP7Modules[module]);
+    }
+
+    mP7Trace->Trace_Managed(0,
+                            level,
+                            mP7Modules[module],
+                            line,
+                            file.c_str(),
+                            function.c_str(),
+                            message.c_str());
+}
---
+++ Logger/Logger.hpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <map>
+#include "P7_Trace.h"
+
+
+#define lmDebug() Logger::Instance().Log(EP7TRACE_LEVEL_DEBUG, __LINE__, __FILE__, __FUNCTION__, LOG_MODULE_NAME)
+#define lmInfo() Logger::Instance().Log(EP7TRACE_LEVEL_INFO, __LINE__, __FILE__, __FUNCTION__, LOG_MODULE_NAME)
+#define lmWarning() Logger::Instance().Log(EP7TRACE_LEVEL_WARNING, __LINE__, __FILE__, __FUNCTION__, LOG_MODULE_NAME)
+#define lmError() Logger::Instance().Log(EP7TRACE_LEVEL_ERROR, __LINE__, __FILE__, __FUNCTION__, LOG_MODULE_NAME)
+#define lmCritical() Logger::Instance().Log(EP7TRACE_LEVEL_CRITICAL, __LINE__, __FILE__, __FUNCTION__, LOG_MODULE_NAME)
+#define lDebug() Logger::Instance().Log(EP7TRACE_LEVEL_DEBUG, __LINE__, __FILE__, __FUNCTION__)
+#define lInfo() Logger::Instance().Log(EP7TRACE_LEVEL_INFO, __LINE__, __FILE__, __FUNCTION__)
+#define lWarning() Logger::Instance().Log(EP7TRACE_LEVEL_WARNING, __LINE__, __FILE__, __FUNCTION__)
+#define lError() Logger::Instance().Log(EP7TRACE_LEVEL_ERROR, __LINE__, __FILE__, __FUNCTION__)
+#define lCritical() Logger::Instance().Log(EP7TRACE_LEVEL_CRITICAL, __LINE__, __FILE__, __FUNCTION__)
+
+
+class Logger
+{
+private:
+    class LoggerStream {
+    public:
+        LoggerStream(const eP7Trace_Level level,
+                     const std::string& tag,
+                     const uint16_t line,
+                     const std::string& file,
+                     const std::string& function);
+        ~LoggerStream();
+
+        template <typename T>
+        LoggerStream& operator<<(const T& value) {
+            mStream << value;
+            return *this;
+        }
+
+    private:
+        eP7Trace_Level     mLevel;
+        std::string        mTag;
+        uint16_t           mLine;
+        std::string        mFile;
+        std::string        mFunction;
+        std::ostringstream mStream;
+    };
+
+    friend class LoggerStream;
+
+public:
+    LoggerStream Log(const eP7Trace_Level level,
+                     const uint16_t line,
+                     const std::string& file,
+                     const std::string& function,
+                     const std::string& module = "Undefined");
+
+public:
+    static Logger& Instance();
+
+private:
+    Logger();
+    ~Logger();
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+
+private:
+    void Init();
+    void P7Log(const eP7Trace_Level level,
+               const std::string& module,
+               const uint16_t line,
+               const std::string& file,
+               const std::string& function,
+               const std::string& message);
+
+private:
+    std::unique_ptr<IP7_Client>               mP7Client;
+    std::unique_ptr<IP7_Trace>                mP7Trace;
+    std::map<std::string, IP7_Trace::hModule> mP7Modules;
+};
